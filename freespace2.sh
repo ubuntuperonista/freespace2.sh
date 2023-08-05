f425f3229b51e06990a8cbf237515feafe7a085e
ubuntuperonista <30541016+ubuntuperonista@users.noreply.github.com>
2023-08-05 01:05:04 -0300

    Create freespace2.sh

---
+++ freespace2.sh
@@ -0,0 +1,12 @@
+#Ejecuta FreeSpace2
+cd .FreeSpace2/
+#pelado:
+#./fs2_open_3_8_0_x64.AppImage
+#Spacepòrt
+./fs2_open_3_8_0_x64.AppImage -mod fsport-mediavps_2014,fsport,mediavps_2014 -nomotiondebris -missile_lighting -3dshockwave -post_process -soft_particles -fxaa -nolightshafts -fb_explosions -fb_thrusters -no_deferred -enable_shadows -snd_preload -ambient_factor 105 -no_emissive_light -spec_exp 9 -spec_point 0.3 -spec_static 0.8 -spec_tube 0.7 -ogl_spec 120
+
+#campaña original con mods HQ
+#./fs2_open_3_8_0_x64.AppImage  -mod MediaVPs_2014 -nomotiondebris -missile_lighting -3dshockwave -post_process -soft_particles -fxaa -nolightshafts -fb_explosions -fb_thrusters -no_deferred -enable_shadows -snd_preload -ambient_factor 105 -no_emissive_light -spec_exp 9 -spec_point 0.3 -spec_static 0.8 -spec_tube 0.7 -ogl_spec 120
+
+# sin cosas 
+# -mod MediaVPs_2014 -3dshockwave -post_process -targetinfo -orbradar -rearm_timer -ship_choice_3d -weapon_choice_3d -warp_flash -ambient_factor 75 -spec_exp 11 -spec_point 0.6 -spec_static 0.8 -spec_tube 0.4 -ogl_spec 60
